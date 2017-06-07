406cd0a0b6807cb3f9175e30bff3cac4c09e32ea
saskiamaulansyah <saskiamaulansyah@gmail.com>
2017-06-07 08:09:19 +0700

    Initial commit

---
+++ app/src/main/java/xir4n30/remidiuts/telkomschid/remidiuts/MainActivity.java
@@ -0,0 +1,13 @@
+package xir4n30.remidiuts.telkomschid.remidiuts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
